38b4fc6af15a8ef99f4fd0cb7db9b03990680f11
yakob solomon <99880602+yakobsolo@users.noreply.github.com>
2023-01-03 20:54:15 +0300

    Time: 126 ms (86.88%), Space: 13.9 MB (93.63%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.py
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.py
@@ -4,10 +4,11 @@ class Solution:
         res = 0 
         
         l = 0
+        maxf= 0
         for r in range(len(s)):
             letthash[s[r]] = 1 + letthash.get(s[r], 0)
-            
-            while (r-l+1) - max(letthash.values()) > k:
+            maxf = max(maxf, letthash[s[r]])
+            while (r-l+1) - maxf > k:
                 letthash[s[l]] -= 1
                 l +=1
         
