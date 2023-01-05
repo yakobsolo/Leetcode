3ecdbb1ffa344d9873aeb64109f04b9518bd6662
yakob solomon <99880602+yakobsolo@users.noreply.github.com>
2023-01-05 19:30:14 +0300

    Time: 62 ms (16.36%), Space: 13.9 MB (50.48%) - LeetHub

---
+++ 2351-first-letter-to-appear-twice/2351-first-letter-to-appear-twice.py
@@ -0,0 +1,8 @@
+class Solution:
+    def repeatedCharacter(self, s: str) -> str:
+        hashmap = {}
+        for i in s:
+            hashmap[i] = 1+ hashmap.get(i, 0)
+            if hashmap[i] == 2:
+                return i
+        
