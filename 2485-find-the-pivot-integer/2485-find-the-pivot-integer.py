362fd166c09e9f4b9a0671ae0382206102f2674c
yakob solomon <99880602+yakobsolo@users.noreply.github.com>
2023-01-27 18:13:07 +0300

    Time: 47 ms (85.33%), Space: 13.9 MB (59.02%) - LeetHub

--- 2485-find-the-pivot-integer/2485-find-the-pivot-integer.py
+++ 2485-find-the-pivot-integer/2485-find-the-pivot-integer.py
@@ -1,34 +1,17 @@
 class Solution:
     def pivotInteger(self, n: int) -> int:
-        suml = 1
-        sumr = n
-        num = n
-        if n ==1 :
+        tot = n*(n+1)/2
+        temp = 0
+        if n == 1:
             return n
-        for l in range(2, n):
+        for i in range(1, n):
             
             
-            
-            
-            
-                
-            if sumr> suml:
-                suml += l
-            if sumr == suml and l == num:
-                return l
-            if suml >= sumr:
-                num = num -1
-                sumr += num
-            
-            
-            
-            # 2 3 4 5 6 7  
-            # l 15  
-            # r 21
-            # l 6
-            # num 6
-        return -1        
-            
+            temp += i
+            if tot == temp:
+                return i
+            tot -= i
+        return -1
             
             
             
