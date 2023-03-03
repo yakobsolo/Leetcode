1f28803f3d555e2a6b559e02898e2757cf6360fc
yakob solomon <99880602+yakobsolo@users.noreply.github.com>
2023-03-03 14:38:48 +0300

    Time: 455 ms (82.87%), Space: 15.5 MB (16.86%) - LeetHub

--- 0875-koko-eating-bananas/0875-koko-eating-bananas.py
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.py
@@ -1,10 +1,11 @@
 class Solution:
     def minEatingSpeed(self, piles: List[int], h: int) -> int:
-        if len(piles) == h:
-            return max(piles)
+#         if len(piles) == h:
+#             return max(piles)
         
-        if h >= sum(piles):
-            return 1
+#         if h >= sum(piles):
+#             return 1
+        
         min_k = k_max = max(piles)
         k_low = 1
         
@@ -14,18 +15,11 @@ class Solution:
 
             mid = (k_low + k_max) // 2
             for i in range(len(piles)):
-                if piles[i] > mid:
-                    temp = piles[i]//mid
-                    mod = piles[i]%mid
-                    hour += temp
-                    if mod != 0:
-                        hour+=1
-                else:
-                    hour +=1 
+                hour+= ceil(piles[i]/mid) 
             
             if hour > h:
                 k_low = mid+1
-            if hour <= h:
+            else:
                 min_k = min(min_k, mid)
                 k_max = mid-1
         return min_k
