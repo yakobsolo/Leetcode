c56d89c094927ee22204dedb77c967197ee4d459
yakob solomon <99880602+yakobsolo@users.noreply.github.com>
2023-03-06 12:13:59 +0300

    Time: 33 ms (60.08%), Space: 13.9 MB (10.23%) - LeetHub

--- 0119-pascals-triangle-ii/0119-pascals-triangle-ii.py
+++ 0119-pascals-triangle-ii/0119-pascals-triangle-ii.py
@@ -1,17 +1,32 @@
 class Solution:
     def getRow(self, rowIndex: int) -> List[int]:
-        pascalTriangle = [[1]]
+        pascalTriangle = [1]
         if rowIndex == 0:
             return [1]
         
-        for i in range(rowIndex):
-            temp = [0]+pascalTriangle[-1]+[0]
+        def recursion(rowIndex):
+            nonlocal pascalTriangle
             row = []
+            if rowIndex == 0:
+                return pascalTriangle
             
-            for j in range(len(temp)-1):
-                row.append(temp[j]+temp[j+1])   
-            if len(row) == rowIndex+1:
-                return row
-            pascalTriangle.append(row)
+            temp = [0]+pascalTriangle+[0]
+            r = 0
+            leng = len(temp) -1
+            def recurtiontemp(r):
+                if r == leng:
+                    return row
+                row.append(temp[r] + temp[r+1])
+                return recurtiontemp(r+1)
+            
+            row = recurtiontemp(r)
+           
+          
+            pascalTriangle=row
+            
+            return recursion(rowIndex-1)
        
+    
+        recursion(rowIndex)
+        return pascalTriangle
         
