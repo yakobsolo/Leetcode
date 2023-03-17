ec8d5d6571269f01242f8f595138a9befdf5c901
yakob solomon <99880602+yakobsolo@users.noreply.github.com>
2023-03-17 10:41:49 +0300

    Time: 34 ms (89.32%), Space: 14.2 MB (7.09%) - LeetHub

--- 0090-subsets-ii/0090-subsets-ii.py
+++ 0090-subsets-ii/0090-subsets-ii.py
@@ -1,16 +1,19 @@
 class Solution:
     def subsetsWithDup(self, nums: List[int]) -> List[List[int]]:
         nums.sort()
-        ans = set()
+        ans = []
         def issubsets(index, path):
-            if index >= len(nums):
+            if index == len(nums):
                 
-                ans.add(tuple(path[::]))
+                ans.append(path[:])
                 return 
             
             
             path.append(nums[index])
             issubsets(index+1, path)
+            while index < len(nums)-1 and nums[index] ==  nums[index+1]:
+                index+=1
+                
             path.pop()
             issubsets(index+1,path)
         issubsets(0, [])
