b90ebeff4a4c2cf876f03c3bed9f8af287dde1e0
yakob solomon <99880602+yakobsolo@users.noreply.github.com>
2023-03-26 16:23:00 +0300

    Time: 111 ms (55.91%), Space: 13.9 MB (83.25%) - LeetHub

--- 1239-maximum-length-of-a-concatenated-string-with-unique-characters/1239-maximum-length-of-a-concatenated-string-with-unique-characters.py
+++ 1239-maximum-length-of-a-concatenated-string-with-unique-characters/1239-maximum-length-of-a-concatenated-string-with-unique-characters.py
@@ -1,34 +1,18 @@
 class Solution:
     def maxLength(self, arr: List[str]) -> int:
-        ans = 0 
-        def unique(path, concate):
-            
-            
-            
-            count = "".join(path)
-            if len(set(count)) != len(count) or len(set(concate)) != len(concate):
-                return False
-            for c in concate:
-                if c in count:
-                    return False
-            return True
-        
-        def maxconcat(idx, path):
-            nonlocal ans
-            if idx == len(arr):
-                
+        self.ans = 0 
 
-                strg = "".join(path)
-                if len(set(strg)) != len(strg):
-                    return 
-                ans = max(ans, len(strg))
+        def maxconcat(idx, path):
+            
+            strg = "".join(path)
+            if len(set(strg)) < len(strg):
                 return 
-            
-            if not path or unique(path, arr[idx]):
                 
-                maxconcat(idx+1, path + [arr[idx]])
+            self.ans = max(self.ans, len(strg))
+            for i in range(idx, len(arr)):
+                maxconcat(i+1, path + [arr[i]])
             
-            maxconcat(idx+1, path)
+            
         maxconcat(0, [])
-        return ans
+        return self.ans
                 
