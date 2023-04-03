75837a9958cdf2d4e112f20375db45708dec2e2d
yakob solomon <99880602+yakobsolo@users.noreply.github.com>
2023-04-03 10:19:33 +0300

    Time: 145 ms (79.30%), Space: 18.2 MB (11.05%) - LeetHub

--- 0526-beautiful-arrangement/0526-beautiful-arrangement.py
+++ 0526-beautiful-arrangement/0526-beautiful-arrangement.py
@@ -2,24 +2,28 @@ class Solution:
     def countArrangement(self, N):
         self.visted = 0
         self.count = 0
-        def permutation(perm, idx):
-            if self.visted.bit_count() == N:
-                # for i in range(len(perm)):
-                #     if ((perm[i] % (i+1) != 0) and ((i+1) % perm[i] != 0)):
-                #         return 
-                self.count +=1
-                return
-            
-            for i in range(N):
-                if not (self.visted & (1<<i)):
-                    if (i+1)% idx == 0 or idx%(i +1) == 0:
-                        
-                        perm.append(i+1)
-                        self.visted |= (1<<i)
+        def permutation(idx):
+            if (idx,self.visted) not in memo:
+                if self.visted.bit_count() == N:
+                    # for i in range(len(perm)):
+                    #     if ((perm[i] % (i+1) != 0) and ((i+1) % perm[i] != 0)):
+                    #         return 
+                    # self.count +=1
+                    return 1
+                
+                count = 0
+                for i in range(N):
+                    if not (self.visted & (1<<i)):
+                        if (i+1)% idx == 0 or idx%(i +1) == 0:
 
-                        permutation(perm, idx+1)
-                        perm.pop()
-                        self.visted &= ~(1<<i)
+                            self.visted |= (1<<i)
+                            count += permutation( idx+1)
+                            self.visted &= ~(1<<i)
+                            
+                memo[(idx,self.visted)] = count
+                
+            return memo[(idx,self.visted)]
         
-        permutation([], 1)
-        return self.count
+        memo = {}
+        return permutation( 1)
+        # return self.count
