392e90fac70b75d10f6cc88d1eb874076e023e93
yakob solomon <99880602+yakobsolo@users.noreply.github.com>
2023-04-05 19:13:35 +0300

    Time: 109 ms (36.50%), Space: 15.8 MB (56.58%) - LeetHub

--- 0137-single-number-ii/0137-single-number-ii.py
+++ 0137-single-number-ii/0137-single-number-ii.py
@@ -1,11 +1,20 @@
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
+        binary = [0] * 32
         leng = len(nums)
-        hash = {}
+        negativecount = 0
         for i in range(leng):
-            if nums[i] not in hash:
-                hash[nums[i]] = 1
-            else:
-                hash[nums[i]] +=1
-        sortedhash = sorted(hash.items(), key=lambda x:x[-1])
-        return sortedhash[0][0]
+            if nums[i] < 0: 
+                negativecount +=1
+            num = abs(nums[i])
+            for j in range(num.bit_length()):
+                if num & (1<<j):
+                    binary[j] +=1
+        ans = 0
+        for i in range(32):
+            if binary[i] % 3 != 0:
+                ans += 2**i
+        if negativecount%3 != 0:
+            return -ans
+        else:
+            return ans
