9f5a3fe152f765e8d68fc68de7576f405111eaeb
yakob solomon <99880602+yakobsolo@users.noreply.github.com>
2023-04-14 20:29:40 +0300

    Time: 106 ms (38.85%), Space: 15.5 MB (96.39%) - LeetHub

--- 0797-all-paths-from-source-to-target/0797-all-paths-from-source-to-target.py
+++ 0797-all-paths-from-source-to-target/0797-all-paths-from-source-to-target.py
@@ -1,24 +1,14 @@
 class Solution:
     def allPathsSourceTarget(self, graph: List[List[int]]) -> List[List[int]]:
-        graph_ = defaultdict(list)
-        i= 0
-        for adj in graph:
-            graph_[i] += adj
-            i+=1
+        n = len(graph)-1
         ans = []
-        visted = set()
+        
         def dfs(node, path):
-            
-            path.append(node)
-            visted.add(node)
-            
-            for adj in graph[node]:
-                if adj not in visted:
-                    dfs(adj, path)
-                    top = path.pop()
-                    visted.discard(top)
-                    
-            if node == i-1:
+            if node == n:
                 ans.append(path[:])
-        dfs(0, [])
+                
+            for adj in graph[node]:
+                dfs(adj, path + [adj])
+                    
+        dfs(0, [0])
         return ans
