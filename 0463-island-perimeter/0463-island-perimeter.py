46ecfba6b781b3624efe2d6bba25d79225959b18
yakob solomon <99880602+yakobsolo@users.noreply.github.com>
2023-04-17 10:04:40 +0300

    Time: 626 ms (34.33%), Space: 20.9 MB (21.95%) - LeetHub

--- 0463-island-perimeter/0463-island-perimeter.py
+++ 0463-island-perimeter/0463-island-perimeter.py
@@ -4,12 +4,19 @@ class Solution:
         rowleng = len(grid)
         colleng = len(grid[0])
         
+        dl = [-1, 0, 1, 0]
+        dr = [0, 1, 0, -1]
+        def dfs(r, c):
+            if not ((0<= r<rowleng) and (0<= c< colleng)) or not grid[r][c]: return 1
+            if grid[r][c] == -1: return 0
+            
+            grid[r][c] = -1
+            count = 0
+            for i in range(4):
+                count += dfs(r + dl[i], c+dr[i])
+            return count
+        
         for r in range(rowleng):
             for c in range(colleng):
-                if grid[r][c]:
-                    peri+=4
-                    if r>0 and grid[r-1][c]:
-                        peri-=2
-                    if c>0 and grid[r][c-1]:
-                        peri-=2
-        return peri
+                if grid[r][c]: return dfs(r, c)
+                
