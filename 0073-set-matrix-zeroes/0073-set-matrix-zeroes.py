b11c95619c1f809781048557d13e8b614300a544
yakob solomon <99880602+yakobsolo@users.noreply.github.com>
2023-04-22 22:39:08 +0300

    Time: 113 ms (99.69%), Space: 14.8 MB (23.75%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.py
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.py
@@ -5,48 +5,19 @@ class Solution:
         """
         rowleng = len(matrix)
         colleng = len(matrix[0])
+        row, col = set(), set()
+        
         for r in range(rowleng):
             for c in range(colleng):
                 if matrix[r][c] == 0:
-                    i, j = r, c
-                    
-                    while 0<= i< rowleng and 0<= j < colleng:
-                        
-                        if i!= r and matrix[i][j] != 0 and matrix[i][j] != "0":
-                            matrix[i][j] = "0"
-                        elif i == r:
-                            matrix[i][j] = "0"
-                        i -=1
-                        
-                    i, j = r, c
-                    while 0<= i< rowleng and 0<= j < colleng:
-                        
-                        if i!=r and matrix[i][j] != 0 and matrix[i][j] != "0":
-                            matrix[i][j] = "0"
-                        elif i == r:
-                            matrix[i][j] = "0"
-                        i +=1
-                        
-                    i, j = r, c
-                    while 0<= i< rowleng and 0<= j < colleng:
-                        
-                        if j!= c and matrix[i][j] != 0 and matrix[i][j] != "0":
-                            matrix[i][j] = "0"
-                        elif j == c:
-                            matrix[i][j] = "0"
-                        j -=1
-                        
-                    i, j = r, c
-                    while 0<= i< rowleng and 0<= j < colleng:
-                        
-                        if j!= c and matrix[i][j] != 0 and matrix[i][j] != "0":
-                            matrix[i][j] = "0"
-                        elif j == c:
-                            matrix[i][j] = "0"
-                        j +=1
-        for r in range(rowleng):
-            for c in range(colleng):
-                if matrix[r][c] == "0":
-                    matrix[r][c] = 0
-                    
-            
+                    row.add(r)
+                    col.add(c)
+        for i in row:
+            matrix[i] = [0]* colleng
+       
+
+        for j in col:
+            for r in range(rowleng):
+               
+                matrix[r][j] = 0
+                
