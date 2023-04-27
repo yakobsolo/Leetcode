2cb9b1f004afd6d6b85f0f9c3272de1f6350c115
yakob solomon <99880602+yakobsolo@users.noreply.github.com>
2023-04-27 18:20:56 +0300

    Time: 737 ms (27.78%), Space: 18 MB (6.43%) - LeetHub

--- 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.py
+++ 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.py
@@ -1,47 +1,38 @@
 class Solution:
     def shortestPathBinaryMatrix(self, grid: List[List[int]]) -> int:
         q = deque()
-        prev = defaultdict(tuple)
-        def shortestpath():
+        self.lev = 0
+        vis_cell = set()
+        dl = [-1,-1,-1,0, 1,1,1,0]
+        dr = [-1, 0,1,1,1,0,-1,-1]
+        
+        def bfs():
             while q:
-                node = q.popleft()
-                if node == (len(grid)-1, len(grid[0])-1):
+                q_len = len(q)
+                if (len(grid)-1, len(grid[0])-1) in vis_cell:
                     break
-                r, c = node
+                flag = 0
+                for _ in range(q_len):
+                    node = q.popleft()
+                    r, c = node
+                    for i in range(8):
+                        j = r + dl[i]
+                        k = c + dr[i]
+                        if j<0 or j==len(grid) or k<0 or k== len(grid[0]) or grid[j][k] != 0:
+                            continue
+                        if (j,k) not in vis_cell:
+                            flag =1
+                            vis_cell.add((j,k))
+                            q.append((j,k))
+                if not flag:
+                    self.lev = -2
+                else:
+                    self.lev+=1
                 
-                dl = [-1, -1, -1, 0, 1, 1, 1, 0]
-                dr = [-1, 0, 1, 1, 1, 0, -1, -1]
-                
-                for i in range(8):
-                    
-                    j= r + dl[i]
-                    k = c+ dr[i]
-                    
-                    if not (0<= j<len(grid) and 0<=k< len(grid[0])) or grid[j][k] !=  0:
-                        continue
-                    q.append((j, k))
-                    prev[(j, k)] = (r, c)
-                    grid[j][k] = 1
-
-                
-            
-            
-        if grid[0][0] == 0: 
-            q.append((0,0))
-            prev[(0, 0)] = (-1, -1)
-            grid[0][0] = 1
-            shortestpath()
+        if grid[0][0] != 0:
+            return -1
         else:
-            return -1
-        
-        if (len(grid)-1, len(grid[0])-1) not in prev:
-            return -1
-        
-        cur = (len(grid)-1, len(grid[0])-1)
-        ans = []
-        
-        while cur != (-1, -1):
-            ans.append(cur)
-            cur = prev[cur]
-        return len(ans)
-    
+            q.append((0, 0))
+            vis_cell.add((0, 0))
+            bfs()
+        return self.lev+1
