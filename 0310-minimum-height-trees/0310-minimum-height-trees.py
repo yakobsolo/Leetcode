51b6378a82a2105ce709cfcf781ca5998b6c9bef
yakob solomon <99880602+yakobsolo@users.noreply.github.com>
2023-05-12 22:31:44 +0300

    Time: 541 ms (40.06%), Space: 26.6 MB (32.17%) - LeetHub

--- 0310-minimum-height-trees/0310-minimum-height-trees.py
+++ 0310-minimum-height-trees/0310-minimum-height-trees.py
@@ -1,7 +1,6 @@
 class Solution:
     def findMinHeightTrees(self, n: int, edges: List[List[int]]) -> List[int]:
-        if n== 1:
-            return [0]
+        
         graph=defaultdict(list)
         indegree = [0] *n 
         for p, c in edges:
@@ -12,37 +11,23 @@ class Solution:
         
         q = deque()
         for i in range(n):
-            if indegree[i] == 1: q.append(i)
+            if indegree[i] <= 1: q.append(i)
          
         ans = []
-        def bfs():
-            done = set()
-            while q:
-                nextprocess = set()
-                q_len =len(q)
-                
-                for _ in range(q_len):
-                
-                    top = q.popleft()
-                    done.add(top)
-                   
-                    for adj in graph[top]:
-                        if adj in q:
-                            ans.append(top)
-                            ans.append(adj)
-                            return
-                        
-                        if adj not in done:
-                            indegree[adj] -=1
-                            if indegree[adj] == 0:
-                                ans.append(adj)
-                                return 
-                            if indegree[adj] == 1:
-                                nextprocess.add(adj)
-                                
-                for next in nextprocess:
-                    q.append(next)
-                 
+        while q:
+            temp = []
+            q_len =len(q)
 
-        bfs()
+            for _ in range(q_len):
+
+                top = q.popleft()
+                temp.append(top)
+
+                for adj in graph[top]:
+                    indegree[adj] -=1
+                    if indegree[adj] == 1:
+                        q.append(adj)
+
+            ans = temp
+
         return ans
