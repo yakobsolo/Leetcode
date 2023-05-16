4540317dbfb2921f3cbcdcd9a7e750e2e8178610
yakob solomon <99880602+yakobsolo@users.noreply.github.com>
2023-05-16 23:44:18 +0300

    Time: 2044 ms (51.67%), Space: 105 MB (85.61%) - LeetHub

--- 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.py
+++ 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.py
@@ -1,25 +1,36 @@
 class Solution:
+        
+    
+        
     def validPath(self, n: int, edges: List[List[int]], source: int, destination: int) -> bool:
-        graph = defaultdict(list)
-        
-        leng = len(edges)
-        for i in range(leng):
-            graph[edges[i][0]].append(edges[i][1])
-            graph[edges[i][1]].append(edges[i][0])
-        traversed = set()  
-        
-        def dfs(node):
-            if node == destination:
-                return True
-            
-            traversed.add(node)
-            print(node)
-            for neighbor in graph[node]:
-                if neighbor not in traversed:
-                    if dfs(neighbor):
-                        return True
-                
-        return dfs(source)
-         
-                
-        
+        root = [i for i in range(n)]
+        rank = [1 for _ in range(n)]
+
+        def find(vert):
+            if root[vert] == vert:
+                return vert
+            root[vert] = find(root[vert])
+            return root[vert]
+
+        def union(v1, v2):
+            r1 = find(v1)
+            r2 = find(v2)
+            if r1 != r2:
+                if rank[r1] > rank[r2]:
+                    root[r2] = r1
+                elif rank[r1] < rank[r2]:
+                    root[r1] = r2
+                else:
+                    root[r2] = r1
+                    rank[r1] += 1
+       
+
+        for e1, e2 in edges:
+            union(e1, e2)
+
+        root1 = find(source)
+        root2 = find(destination)
+
+        return root1 == root2
+
+
