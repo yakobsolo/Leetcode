4d2337183a6f62abcdcca2a8751f645e5b7b943e
yakob solomon <99880602+yakobsolo@users.noreply.github.com>
2023-05-17 11:16:09 +0300

    Time: 1861 ms (78.77%), Space: 104.9 MB (91.39%) - LeetHub

--- 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.py
+++ 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.py
@@ -6,27 +6,28 @@ class Solution:
         root = [i for i in range(n)]
         rank = [1 for _ in range(n)]
 
-        def find(vert):
-            if root[vert] == vert:
-                return vert
-            root[vert] = find(root[vert])
-            return root[vert]
-
-        def union(v1, v2):
-            r1 = find(v1)
-            r2 = find(v2)
-            if r1 != r2:
-                if rank[r1] > rank[r2]:
-                    root[r2] = r1
-                elif rank[r1] < rank[r2]:
-                    root[r1] = r2
+        def find(node):
+            if root[node] == node:
+                return node
+            root[node] = find(root[node])
+            return root[node]
+        
+        
+        def union(x, y):
+            
+            par1 = find(x)
+            par2 = find(y)
+            if par1 != par2:
+                if rank[par1] > rank[par2]:
+                    root[par2] = par1
+                    rank[par1] += 1
                 else:
-                    root[r2] = r1
-                    rank[r1] += 1
-       
+                    root[par1] = par2
+                    rank[par2] +=1
+                    
 
-        for e1, e2 in edges:
-            union(e1, e2)
+        for p, c in edges:
+            union(p, c)
 
         return find(source) ==  find(destination)
 
