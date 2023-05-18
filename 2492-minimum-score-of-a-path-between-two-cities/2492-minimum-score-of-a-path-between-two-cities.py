2f2265e0bfcdc657dec924fe79f2a0c8e8e15d84
yakob solomon <99880602+yakobsolo@users.noreply.github.com>
2023-05-18 19:59:10 +0300

    Time: 1592 ms (84.61%), Space: 61.1 MB (81.29%) - LeetHub

--- 2492-minimum-score-of-a-path-between-two-cities/2492-minimum-score-of-a-path-between-two-cities.py
+++ 2492-minimum-score-of-a-path-between-two-cities/2492-minimum-score-of-a-path-between-two-cities.py
@@ -2,31 +2,31 @@ class Solution:
     def minScore(self, n: int, roads: List[List[int]]) -> int:
         rep , rank ={}, {}
         rep = {v:v for v in range(1, n+1)}
-        rank = {v:1 for v in range(1, n+1)}
+        rank = {v:inf for v in range(1, n+1)}
         
         def find(n):
             if n == rep[n]: return n
             rep[n] = find(rep[n])
             return rep[n]
         
-        def union(x, y):
+        def union(x, y, d):
             par1 = find(x)
             par2 = find(y)
             
             if rank[par1] <= rank[par2]:
                 rep[par2] = par1
-                rank[par1] +=1
+                rank[par1] = min(rank[par1], d)
             else:
                 rep[par1] = par2
-                rank[par2] +=1
+                rank[par2] = min(rank[par2], d)
                 
                         
         for a, b, d in roads:
-            union(a, b)
-            
-        ans  = inf
-        for a, b, d in roads:
-            if find(a) == find(b) == find(1):
-                ans = min(ans, d)
+            union(a, b, d)
+        return rank[find(n)]
+        # ans  = inf
+        # for a, b, d in roads:
+        #     if find(a) == find(b) == find(1):
+        #         ans = min(ans, d)
         return ans
             
