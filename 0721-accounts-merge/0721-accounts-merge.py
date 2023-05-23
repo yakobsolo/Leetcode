3607ad12f73044ffe65ec3fd0ce3ea3e2f9ed858
yakob solomon <99880602+yakobsolo@users.noreply.github.com>
2023-05-23 21:59:53 +0300

    Time: 2919 ms (5.01%), Space: 21.1 MB (38.70%) - LeetHub

---
+++ 0721-accounts-merge/0721-accounts-merge.py
@@ -0,0 +1,45 @@
+
+class Solution:
+    def accountsMerge(self, accounts: List[List[str]]) -> List[List[str]]:
+        
+        hash = defaultdict(set)
+        leng = len(accounts)
+        
+        for i in range(leng):
+            hash[(i, accounts[i][0])] = set(accounts[i][1:])
+                
+        rep = {(i,j): (i,j) for i, j in hash}
+    
+        def find(n):
+            if n == rep[n]: return n
+            rep[n] = find(rep[n])
+            return rep[n]
+        
+        def union(x, y):
+            
+            par1 = find(x)
+            par2 = find(y)
+            
+            rep[par1] = par2
+            
+        for key in hash:
+            for k in hash:
+                if key != k and key[1] == k[1]:
+                    for i in hash[key]:
+                        if i in hash[k]:
+                            union(key, k)
+                        
+        
+        ans = []
+        for par in rep:
+            x = find(par)
+            hash[x].update(hash[par])
+            
+        for par in rep:
+            
+            if par == rep[par]:
+                arr = [par[1]] + list(sorted(list(hash[par])))
+                ans.append(arr)
+                
+        return ans
+              
