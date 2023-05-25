1c7df80335c107a8e441032cae340a492f4888f9
yakob solomon <99880602+yakobsolo@users.noreply.github.com>
2023-05-25 22:33:29 +0300

    Time: 227 ms (87.11%), Space: 18 MB (13.40%) - LeetHub

--- 1053-previous-permutation-with-one-swap/1053-previous-permutation-with-one-swap.py
+++ 1053-previous-permutation-with-one-swap/1053-previous-permutation-with-one-swap.py
@@ -1,15 +1,16 @@
 class Solution:
     def prevPermOpt1(self, arr: List[int]) -> List[int]:
-        N = len(arr)
-        dq = deque([N - 1])
-        for i in range(N - 2, -1, -1):
-            if arr[i] < arr[i + 1]:
-                dq.appendleft(i)
-            elif arr[i] == arr[i + 1]:
-                dq[0] = i
+        n = len(arr)
+        q = deque([n-1])
+        
+        for i in range(n-2, -1, -1):
+            if arr[i] < arr[i+1]:
+                q.appendleft(i)
+            elif arr[i] == arr[i+1]:
+                q[0] = i
             else:
-                while arr[dq[-1]] >= arr[i]:
-                    dq.pop()
-                arr[i], arr[dq[-1]] = arr[dq[-1]], arr[i]
+                while arr[i] <= arr[q[-1]]:
+                    q.pop()
+                arr[i], arr[q[-1]] = arr[q[-1]], arr[i]
                 break
         return arr
