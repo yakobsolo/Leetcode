cd03b0b2a921c335d9f63c0a52936727e3a14ff1
yakob solomon <99880602+yakobsolo@users.noreply.github.com>
2023-05-29 11:29:07 +0300

    Time: 53 ms (6.72%), Space: 16.3 MB (9.57%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -0,0 +1,10 @@
+class Solution:
+    def __init__(self):
+        self.memo = {}
+    def climbStairs(self, n: int) -> int:
+        if n< 3:
+            return n
+        
+        if n not in self.memo:
+            self.memo[n] = self.climbStairs(n-1) + self.climbStairs(n-2)
+        return self.memo[n]
