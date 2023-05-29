102326c14369247f00b7b83711b29a8772b628e8
yakob solomon <99880602+yakobsolo@users.noreply.github.com>
2023-05-29 14:53:12 +0300

    Time: 65 ms (6.04%), Space: 16.4 MB (15.22%) - LeetHub

--- 0198-house-robber/0198-house-robber.py
+++ 0198-house-robber/0198-house-robber.py
@@ -1,6 +1,6 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
-        
+        n = len(nums)
         def dp(i):
             if i == 0:
                 return nums[i]
@@ -10,5 +10,6 @@ class Solution:
             if memo[i] == -1:
                 memo[i] = max(dp(i-1), dp(i-2) + nums[i])
             return memo[i]
-        memo = [-1]*len(nums)
-        return dp(len(nums) -1)
+        memo = [-1]*n
+        
+        return dp(n -1)
