79f1db4cf84b85f68fe460f387fb922b3b4519ce
yakob solomon <99880602+yakobsolo@users.noreply.github.com>
2023-05-30 02:22:56 +0300

    Time: 51 ms (21.25%), Space: 16.4 MB (17.51%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.py
+++ 0213-house-robber-ii/0213-house-robber-ii.py
@@ -1,26 +1,19 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
         
-        memo1 = {}
-        memo2 = {}
-        def dp1(i):
-            if i == 0:
-                return nums[i]
-            if i == 1:
-                return max(nums[0], nums[1])
-            
-            if i not in memo1:
-                memo1[i] = max(dp1(i-1), dp1(i-2) + nums[i])
-            return memo1[i]
-        def dp2(i):
-            if i == 1:
-                return nums[i]
-            if i == 2:
-                return max(nums[1], nums[2])
-            
-            if i not in memo2:
-                memo2[i] = max(dp2(i-1), dp2(i-2) + nums[i])
-            return memo2[i]
-        n = len(nums)
-        if n<2: return nums[0]
-        return max(dp1(n-2), dp2(n-1))
+        if len(nums) == 1: return nums[0]
+        def dp(l,r):
+            if r < l: return 0
+            if l == r:return nums[l]
+            if r not in memo:
+                memo[r] = max(dp(l,r-1),dp(l,r-2)+nums[r])
+            return memo[r]
+        memo = {}
+        n= len(nums)
+        res =dp(1, n-1)
+        memo = {}
+        res = max(res,dp(0,n-2))
+        return res
+    
+    
+    
