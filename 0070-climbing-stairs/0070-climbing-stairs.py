7a24eb4d32b0b7097288ac81e15c604c1a3e4720
yakob solomon <99880602+yakobsolo@users.noreply.github.com>
2023-05-29 11:37:39 +0300

    Time: 51 ms (12.97%), Space: 16.4 MB (9.57%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.py
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -1,10 +1,25 @@
 class Solution:
-    def __init__(self):
-        self.memo = {}
+   
     def climbStairs(self, n: int) -> int:
-        if n< 3:
-            return n
         
-        if n not in self.memo:
-            self.memo[n] = self.climbStairs(n-1) + self.climbStairs(n-2)
-        return self.memo[n]
+        memo = {}
+        def dp(i):
+            if i == n:
+                return 1
+            if i >n:
+                return 0
+            if i not in memo:
+                left = dp(i+1)
+                right = dp(i+2)
+                memo[i] = left + right
+                
+            
+            return memo[i]
+        return dp(0)
+        
+#         if n< 3:
+#             return n
+        
+#         if n not in self.memo:
+#             self.memo[n] = self.climbStairs(n-1) + self.climbStairs(n-2)
+#         return self.memo[n]
