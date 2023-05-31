095347d6ed8084a34e1fc0c55c47e7ae53fa0bf0
yakob solomon <99880602+yakobsolo@users.noreply.github.com>
2023-05-31 19:55:04 +0300

    Time: 45 ms (40.01%), Space: 16.3 MB (43.78%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.py
+++ 0062-unique-paths/0062-unique-paths.py
@@ -1,19 +1,20 @@
 class Solution:
     def uniquePaths(self, m: int, n: int) -> int:
-        visted = set()
-        memo = {}
-        dl = [0, -1]
-        dr = [-1, 0]
-        def dp(cell):
-            if cell == (0, 0): return 1
-            i, j  = cell
+        # dp = [0 for i in range(m) for j in range(n)]
+        dp = []
+        for i in range(m):
+            arr = [0] *n
+            dp.append(arr)
+        dp[-1][-1] = 1
+        
+        def lookup(r, c):
+            if 0<=r<m and 0<=c<n: return dp[r][c]
+            return 0
+        for i in range(m-1, -1, -1):
             
-            if i<0 or i>= m or j< 0 or j>=n: return 0
-            if (i, j) not in memo:
-                memo[(i,j)] = dp((i + dl[0], j + dr[0])) + dp((i + dl[1], j+dr[1]))
+            for j in range(n-1, -1, -1):
+                dp[i][j] += lookup(i+1, j) + lookup(i, j+1)
             
-            return memo[(i,j)]
-
-        return dp((m-1, n-1))
+        return dp[0][0]
             
             
