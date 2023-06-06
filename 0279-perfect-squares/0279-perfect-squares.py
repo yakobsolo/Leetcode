8471d0d1ba474ed2bdcf3460144095fb42ecfa02
yakob solomon <99880602+yakobsolo@users.noreply.github.com>
2023-06-06 19:42:03 +0300

    Time: 3939 ms (60.87%), Space: 16.4 MB (44.96%) - LeetHub

--- 0279-perfect-squares/0279-perfect-squares.py
+++ 0279-perfect-squares/0279-perfect-squares.py
@@ -5,7 +5,6 @@ class Solution:
         dp[0] = 0
         for i in range(1, n+1):
             for c in range(1,floor(i**0.5)+1):
-                
-                dp[i] = min(dp[i], dp[i-c*c]+1)
+                dp[i] = min(dp[i], dp[i-(c*c)]+1)
         return dp[n]
         
