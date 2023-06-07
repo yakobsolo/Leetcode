338fc65a63f58ca078b37c354bfe47c5bc119145
yakob solomon <99880602+yakobsolo@users.noreply.github.com>
2023-06-07 19:18:40 +0300

    Time: 1244 ms (72.04%), Space: 16.3 MB (58.06%) - LeetHub

---
+++ 1601-maximum-number-of-achievable-transfer-requests/1601-maximum-number-of-achievable-transfer-requests.py
@@ -0,0 +1,20 @@
+class Solution:
+    def maximumRequests(self, n: int, requests: List[List[int]]) -> int:
+        path = [0 for i in range(n)]
+        ans = 0
+        leng = len(requests)
+        def backtrack(i, c):
+            nonlocal ans
+            if i == leng:
+                for i in range(n):
+                    if path[i] != 0: return 
+                ans = max(ans,c)
+                return 
+            path[requests[i][0]]-=1
+            path[requests[i][1]] +=1
+            backtrack(i+1,c+1)
+            path[requests[i][0]]+=1
+            path[requests[i][1]] -=1
+            backtrack(i+1, c)
+        backtrack(0, 0)
+        return ans
