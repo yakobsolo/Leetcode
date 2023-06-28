9eb422804244984b381608255866cb6edb39babc
yakob solomon <99880602+yakobsolo@users.noreply.github.com>
2023-06-28 10:19:28 +0300

    Time: 49 ms (55.66%), Space: 16.4 MB (25.28%) - LeetHub

--- 0392-is-subsequence/0392-is-subsequence.py
+++ 0392-is-subsequence/0392-is-subsequence.py
@@ -5,8 +5,7 @@ class Solution:
         if not s: return True
         
         
-        while j<m:
-            
+        for j in range(m):
             if s[i] == t[j]:
                 i+=1
                 if i==n: return True
