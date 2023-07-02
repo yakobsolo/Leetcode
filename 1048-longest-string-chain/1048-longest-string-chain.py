8c1471792cc190cd4bced3951a5f0d0a89da9aa6
yakob solomon <99880602+yakobsolo@users.noreply.github.com>
2023-07-02 19:10:05 +0300

    Time: 1657 ms (19.68%), Space: 17 MB (38.98%) - LeetHub

--- 1048-longest-string-chain/1048-longest-string-chain.py
+++ 1048-longest-string-chain/1048-longest-string-chain.py
@@ -10,6 +10,7 @@ class Solution:
             word = sorted_words[i]
             m = len(sorted_words[i])
             for j in range(i-1, -1, -1):
+                
                 if m == len(sorted_words[j])+1:
                     word2 = sorted_words[j]
                     for k in range(m):
@@ -17,6 +18,6 @@ class Solution:
                         if temp == word2:
                             dp[i] = max(dp[i], dp[j]+1)
                             
-                
+                elif m > len(sorted_words[j]) +1: break
         return max(dp)
                 
