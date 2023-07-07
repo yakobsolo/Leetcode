e5f9080402bf2e8a7fea2c412713da814eca727c
yakob solomon <99880602+yakobsolo@users.noreply.github.com>
2023-07-07 16:59:24 +0300

    Time: 271 ms (92.07%), Space: 16.7 MB (55.49%) - LeetHub

--- 2024-maximize-the-confusion-of-an-exam/2024-maximize-the-confusion-of-an-exam.py
+++ 2024-maximize-the-confusion-of-an-exam/2024-maximize-the-confusion-of-an-exam.py
@@ -1,48 +1,18 @@
 class Solution:
     def maxConsecutiveAnswers(self, answerKey: str, k: int) -> int:
         fcount, tcount = 0, 0
-        maxfcount, maxtcount = 0, 0
+        maxcount= 0
         n = len(answerKey)
         l = 0
         t = k
         for i in range(n):
             if answerKey[i] == 'T': tcount+=1
-                
-            else:
-                if k>0:
-                    k-=1
-                    tcount+=1
-                else:
-                    while k==0:
-                        if answerKey[l] == "F":
-                            k+=1
-                        else: tcount-=1
-                        l+=1
-                    k-=1
-                    
-            maxtcount = max(maxtcount, tcount)
-        l = 0
-        k = t
-        for i in range(n):
-            if answerKey[i] == 'F': fcount+=1
-            else:
-                if k>0:
-                    k-=1
-                    fcount+=1
-                else:
-                    while k==0:
-                        if answerKey[l] == "T":
-                            k+=1
-                        else: fcount-=1
-                        l+=1
-                    k-=1
-                    
-            # print(i, fcount, k)
-            maxfcount = max(maxfcount, fcount)
+            elif answerKey[i] == 'F': fcount+=1
+            while tcount >k and fcount > k:
+                if answerKey[l] == "T": tcount-=1
+                else: fcount-=1
+                l+=1
+            maxcount = max(maxcount, tcount+fcount)
         
-        return max(maxtcount, maxfcount)
-        
+        return maxcount    
             
-                
-                    
-                
