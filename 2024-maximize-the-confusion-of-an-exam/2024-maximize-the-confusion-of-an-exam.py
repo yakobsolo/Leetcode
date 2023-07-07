d279aca33dc289be5ebf52d6d191d8a5155c5775
yakob solomon <99880602+yakobsolo@users.noreply.github.com>
2023-07-07 16:43:00 +0300

    Time: 428 ms (62.50%), Space: 16.7 MB (55.49%) - LeetHub

--- 2024-maximize-the-confusion-of-an-exam/2024-maximize-the-confusion-of-an-exam.py
+++ 2024-maximize-the-confusion-of-an-exam/2024-maximize-the-confusion-of-an-exam.py
@@ -2,12 +2,11 @@ class Solution:
     def maxConsecutiveAnswers(self, answerKey: str, k: int) -> int:
         fcount, tcount = 0, 0
         maxfcount, maxtcount = 0, 0
-        nums = answerKey
         n = len(answerKey)
         l = 0
         t = k
         for i in range(n):
-            if nums[i] == 'T': tcount+=1
+            if answerKey[i] == 'T': tcount+=1
                 
             else:
                 if k>0:
@@ -15,31 +14,31 @@ class Solution:
                     tcount+=1
                 else:
                     while k==0:
-                        if nums[l] == "F":
+                        if answerKey[l] == "F":
                             k+=1
                         else: tcount-=1
                         l+=1
                     k-=1
                     
-            maxfcount = max(maxfcount, tcount)
+            maxtcount = max(maxtcount, tcount)
         l = 0
         k = t
         for i in range(n):
-            if nums[i] == 'F': fcount+=1
+            if answerKey[i] == 'F': fcount+=1
             else:
                 if k>0:
                     k-=1
                     fcount+=1
                 else:
                     while k==0:
-                        if nums[l] == "T":
+                        if answerKey[l] == "T":
                             k+=1
                         else: fcount-=1
                         l+=1
                     k-=1
                     
             # print(i, fcount, k)
-            maxtcount = max(maxtcount, fcount)
+            maxfcount = max(maxfcount, fcount)
         
         return max(maxtcount, maxfcount)
         
