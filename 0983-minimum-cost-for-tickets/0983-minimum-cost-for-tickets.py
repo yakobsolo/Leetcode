e5b9d2599e339bc38b2664123259c97ef6c3ebcd
yakob solomon <99880602+yakobsolo@users.noreply.github.com>
2023-08-08 12:54:07 +0300

    Time: 51 ms (83.38%), Space: 16.4 MB (77.15%) - LeetHub

--- 0983-minimum-cost-for-tickets/0983-minimum-cost-for-tickets.py
+++ 0983-minimum-cost-for-tickets/0983-minimum-cost-for-tickets.py
@@ -3,19 +3,18 @@ class Solution:
         dp = {}
         
         n  = len(days)
+        for i in range(n-1, -1, -1):
         
         
-        def dfs(i):
-            if i == n: return 0
-            if i in dp: return dp[i]
             
             dp[i] = inf
             for d, c in zip([1, 7, 30], costs):
                 j = i
+                
                 while j < n and days[j] < days[i] + d:
                     j +=1
                     
-                dp[i] = min(dp[i], c+ dfs(j))
-            return dp[i]
-        return dfs(0)
+                dp[i] = min(dp[i], c+ dp.get(j, 0))
+            
+        return dp[0]
                 
