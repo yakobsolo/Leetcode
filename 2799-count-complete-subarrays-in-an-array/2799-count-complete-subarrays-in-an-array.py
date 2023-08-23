ce2af0b81f1b4a63637f4d7826252faa533bd88f
yakob solomon <99880602+yakobsolo@users.noreply.github.com>
2023-08-23 21:00:07 +0300

    Time: 99 ms (87.00%), Space: 16.6 MB (70.11%) - LeetHub

--- 2799-count-complete-subarrays-in-an-array/2799-count-complete-subarrays-in-an-array.py
+++ 2799-count-complete-subarrays-in-an-array/2799-count-complete-subarrays-in-an-array.py
@@ -1,16 +1,16 @@
 class Solution:
     def countCompleteSubarrays(self, nums: List[int]) -> int:
-        unique_set = set(nums)
+        init = set(nums)
         count,i = 0 ,0
-        hm = defaultdict(int) #hash map 
+        window = defaultdict(int) #hash map 
 
         for j in nums : 
-            hm[j] += 1
-            while len(unique_set) == len(hm):
-                if hm[nums[i]] == 1 :
-                    hm.pop(nums[i])
+            window[j] += 1
+            while len(window) == len(init):
+                if window[nums[i]] == 1 :
+                    window.pop(nums[i])
                 else : 
-                    hm[nums[i]] -= 1
+                    window[nums[i]] -= 1
                 i += 1
             count += i
         return count
