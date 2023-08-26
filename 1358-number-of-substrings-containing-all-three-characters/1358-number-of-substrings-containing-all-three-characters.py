62bc150f0e89c361a50b6de9f07578ba6f1cc19f
yakob solomon <99880602+yakobsolo@users.noreply.github.com>
2023-08-26 21:26:02 +0300

    Time: 117 ms (99.21%), Space: 16.6 MB (99.74%) - LeetHub

--- 1358-number-of-substrings-containing-all-three-characters/1358-number-of-substrings-containing-all-three-characters.py
+++ 1358-number-of-substrings-containing-all-three-characters/1358-number-of-substrings-containing-all-three-characters.py
@@ -1,33 +1,14 @@
 class Solution:
     def numberOfSubstrings(self, s: str) -> int:
-        hash  = defaultdict(int)
-        count = 0
-        n = len(s)
-        l = 0
+        hash = [-1, -1, -1]
+        ans = 0
         for i, c in enumerate(s):
-            hash[c] +=1
-            while len(hash)>= 3:
-                count += n - i
-                hash[s[l]] -=1
-                if hash[s[l]] == 0:
-                    del hash[s[l]]
-                l+=1
-                
-        return count
-                
+            hash[ord(c) - 97] = i
+            ans += min(hash) +1
             
+             
+               
+        return ans
+                
+         
         
-#         N = len(s)
-#         prev = 0
-#         ans = 0
-#         indexes = {'a': -1, 'b': -1, 'c': -1}
-#         for i, ch in enumerate(s):
-#             prev += 1
-#             min_idx = min(indexes.values())
-#             if min_idx != -1 and s[min_idx] == ch:
-#                 prev -= i - min_idx + 1
-#                 print(prev)
-#                 ans += prev + N - i
-#             indexes[ch] = i
-        
-#         return ans
