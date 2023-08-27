c8b2d765ef519d04728221c905b6fc3e4bfdfe1a
yakob solomon <99880602+yakobsolo@users.noreply.github.com>
2023-08-27 20:50:29 +0300

    Time: 498 ms (48.48%), Space: 20 MB (11.90%) - LeetHub

---
+++ 0792-number-of-matching-subsequences/0792-number-of-matching-subsequences.py
@@ -0,0 +1,25 @@
+class Solution:
+    def numMatchingSubseq(self, s: str, words: List[str]) -> int:
+        
+        dic = defaultdict(list)
+        
+        for i in range(len(s)):
+            dic[s[i]].append(i)
+        
+        ans = 0
+        
+        for word in words:
+            cur = -1
+            is_sub = True
+            
+            for let in word:
+                index = bisect_right(dic[let],cur)
+                
+                if index >= len(dic[let]):
+                    is_sub = False
+                    break
+                
+                cur = dic[let][index]
+            if is_sub:
+                ans += 1
+        return ans
