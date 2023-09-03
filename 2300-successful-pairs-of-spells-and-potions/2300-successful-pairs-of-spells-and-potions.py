2b7bffe91ce8a048a2f269f8ccfaee48820bd35b
yakob solomon <99880602+yakobsolo@users.noreply.github.com>
2023-09-03 09:10:02 +0300

    Time: 919 ms (99.05%), Space: 38.6 MB (99.33%) - LeetHub

--- 2300-successful-pairs-of-spells-and-potions/2300-successful-pairs-of-spells-and-potions.py
+++ 2300-successful-pairs-of-spells-and-potions/2300-successful-pairs-of-spells-and-potions.py
@@ -7,6 +7,5 @@ class Solution:
         for s in spells:
             find = ceil(success/s)
             l = bisect_left(potions, find)
-            print(find, l)
             ans.append(m-l)
         return ans
