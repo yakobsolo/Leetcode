7ce69c1a01898fac1875c773d81d02c5a319f1ef
yakob solomon <99880602+yakobsolo@users.noreply.github.com>
2023-09-08 20:05:16 +0300

    Time: 288 ms (89.68%), Space: 19.2 MB (77.72%) - LeetHub

--- 0718-maximum-length-of-repeated-subarray/0718-maximum-length-of-repeated-subarray.py
+++ 0718-maximum-length-of-repeated-subarray/0718-maximum-length-of-repeated-subarray.py
@@ -2,31 +2,31 @@ class Solution:
     def findLength(self, nums1: List[int], nums2: List[int]) -> int:
         if len(nums1) > len(nums2):
             nums1, nums2 = nums2, nums1
-        def checker(arr1, arr2):
-            n = len(arr1)
-            m = len(arr2)
-
-            max_count = 0
-            for i in range(n):
-
-                j = 0
-                while j < m:
-                    k = i
-                    count = 0
-                    # print(i, j)
-                    while k<n and j< m and arr1[k] == arr2[j]:
-                        k+=1
-                        count += 1
-                        max_count = max(max_count, count)
-                        j += 1
-
-                    if j< m and arr1[i] != arr2[j]:
-                        j+=1
-
-            return max_count
         
-        if len(nums1) == len(nums2):
+        N1  = len(nums1)
+        N2 = len(nums2)
+        max_diff = 0
+        
+         
+        def checker(length):
             
-            return max(checker(nums1, nums2), checker(nums2, nums1))
-        else:
-            return checker(nums1, nums2)
+            subArrays = set(tuple(nums1[p1:p1+length]) for p1 in range(N1-length + 1))
+            # print(subArrays)
+            return any(tuple(nums2[p2:p2+length]) in subArrays for p2 in range(N2-length +1)  )          
+            
+        
+        l, r =  0 , N1
+        
+        while l<=r:
+            mid = l+ (r-l)//2
+            # print(mid, "mid")
+            
+            if checker(mid):
+                # print(mid)
+                l = mid+1
+            else:
+                r = mid -1
+            
+        return r
+
+        
