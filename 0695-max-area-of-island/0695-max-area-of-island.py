4f7033a15719c03044912ac304e765db39a5767c
yakob solomon <yakisolo2009@gmail.com>
2023-09-19 15:49:46 +0300

    Time: 124 ms (74.56%), Space: 20 MB (22.17%) - LeetHub

--- 0695-max-area-of-island/0695-max-area-of-island.py
+++ 0695-max-area-of-island/0695-max-area-of-island.py
@@ -1,23 +1,27 @@
 class Solution:
     def maxAreaOfIsland(self, grid: List[List[int]]) -> int:
-        m , n = len(grid), len(grid[0])
-        maxarea = 0
-        
-        dl = [-1, 0, 1, 0]
-        dr = [0, 1, 0, -1]
-        def dfs(r, c):
-            if not  ((0<= r< m) and (0<=c<n) ) or not grid[r][c] : return 0
-            if grid[r][c] == -1: return 0
+        n , m = len(grid), len(grid[0])
+        max_area = 0
+        visted = set()
+        directions = [(-1, 0), (0, 1), (1, 0), (0, -1)]
+        def dfs(i, j):
             
-            grid[r][c] = -1
-            
-            count = 0
-            for i in range(4):
-                count += dfs(r+dl[i], c+dr[i])
-            return count+1
+            visted.add((i, j))
+            area = 0
+            for k in range(4):
+                r, c = i+directions[k][0], j+directions[k][1]
+                
+                if r>=0 and r<n and c>=0 and c<m and grid[r][c] != 0 and (r, c) not in visted:
+                    area += dfs(r, c)
+                    
+                    
+            return area+1
+                    
         
-        for r in range(m):
-            for c in range(n):
-                if grid[r][c]:
-                    maxarea = max(maxarea, dfs(r, c))
-        return maxarea
+        for i in range(n):
+            for j in range(m):
+                if grid[i][j] != 0 and (i, j) not in visted:
+                    # print(i, j)
+                    max_area = max(max_area, dfs(i, j))
+                    # print(max_area)
+        return max_area
