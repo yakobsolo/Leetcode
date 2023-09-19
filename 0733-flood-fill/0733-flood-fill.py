eaa523585c254e5ace0ca2024d8fbfc1ce18d69e
yakob solomon <yakisolo2009@gmail.com>
2023-09-19 10:32:39 +0300

    Time: 70 ms (85.74%), Space: 16.8 MB (7.26%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.py
+++ 0733-flood-fill/0733-flood-fill.py
@@ -1,27 +1,18 @@
 class Solution:
     def floodFill(self, image: List[List[int]], sr: int, sc: int, color: int) -> List[List[int]]:
-        n = len(image)
-        c = len(image[0])
-        
-        if image[sr][sc] == color:
-            return image
-                
-            
-        dl = [-1, 0, 1,0]
-        dr = [0, 1, 0,-1]
-        
-        def flood(sr, sc, source):
-            
-            if sr< 0 or sc<0 or sr>= n or sc>=c:
-                return 
-            if image[sr][sc] != source:
-                return 
-            
-            for i in range(4):    
-                image[sr][sc] = color
-                flood(sr+dl[i], sc + dr[i], source)
-                    
-                
-        source = image[sr][sc]          
-        flood(sr, sc, source)
+        direction = [(-1, 0), (0, 1), (1, 0), (0, -1)]
+        visted = set()
+        initial = image[sr][sc]
+        n, m = len(image), len(image[0])
+        def dfs(l, r):
+            image[l][r] = color
+            visted.add((l, r))
+            for i in range(4):
+                i, j = l+direction[i][0], r+direction[i][1]
+                print(i, j)
+                if i>=0 and i<n and j>=0 and j<m and ((i, j) not in visted) and image[i][j] == initial:
+                    image[i][j] = color
+                    print(i, j)
+                    dfs(i, j)
+        dfs(sr, sc)
         return image
