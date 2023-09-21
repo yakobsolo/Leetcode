21e902087c41cf7550876342730323fd1df0cb85
yakob solomon <yakisolo2009@gmail.com>
2023-09-21 11:56:33 +0300

    Time: 582 ms (53.98%), Space: 90.7 MB (25.83%) - LeetHub

---
+++ 1020-number-of-enclaves/1020-number-of-enclaves.py
@@ -0,0 +1,58 @@
+class Solution:
+    def numEnclaves(self, grid: List[List[int]]) -> int:
+        directions = [(-1, 0), (0, 1), (1, 0), (0, -1)]
+        n, m = len(grid), len(grid[0])
+        visted = set()
+        
+          
+        def dfs(i, j):
+            if (i<0 or i>=n or j<0 or j>=m) or (i, j)  in visted or grid[i][j] == 0:
+                return 
+            
+            visted.add((i, j))
+            grid[i][j] = 0
+            # print(i,j)
+            # print(grid)
+            for r, c in directions:
+                r, c = i+r, j+c
+                
+                dfs(r, c)
+        
+        
+        
+        
+        
+        
+        for j in range(m):
+            if grid[0][j] == 1 and (0, j) not in visted:
+                dfs(0, j)
+        for j in range(m):
+            if grid[n-1][j] == 1 and (n-1, j) not in visted:
+                dfs(n-1 ,j)
+        for i in range(n):
+            if grid[i][0] == 1 and (i, 0) not in visted:
+                # print(i, '------------')
+                dfs(i, 0)
+                # print(count)
+        for i in range(n):
+            if grid[i][m-1] == 1 and (i, m-1) not in visted:
+                dfs(i, m-1)
+                
+        count_ones = 0
+        
+        for i in range(n):
+            for j in range(m):
+                if grid[i][j] == 1:
+                    count_ones += 1
+                    
+        return count_ones 
+        
+        
+                
+
+                
+            
+                
+                    
+                    
+                
