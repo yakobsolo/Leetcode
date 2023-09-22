5c0c852ee396446f0bff7ed3ac86ce0e22ae903f
yakob solomon <yakisolo2009@gmail.com>
2023-09-22 21:42:39 +0300

    Time: 97 ms (25.87%), Space: 16.5 MB (65.66%) - LeetHub

--- 1129-shortest-path-with-alternating-colors/1129-shortest-path-with-alternating-colors.py
+++ 1129-shortest-path-with-alternating-colors/1129-shortest-path-with-alternating-colors.py
@@ -2,46 +2,56 @@ class Solution:
     def shortestAlternatingPaths(self, n: int, redEdges: List[List[int]], blueEdges: List[List[int]]) -> List[int]:
         
         
-            
-        graph = collections.defaultdict(list)
+        
+        queue = deque()
         ans = [-1]*n
-        ans[0] = 0
+        lev = 1
+        
+        visted = set()
+        graph = defaultdict(list)
         
         for p, c in redEdges:
             graph[p].append((c, "r"))
         for p, c in blueEdges:
             graph[p].append((c, "b"))
         
-        visted = []
-        q = collections.deque()
-        q.append([0, "r"])    
-        q.append([0, "b"])
-        dis = 1
-        def bfs():
-            nonlocal dis
-            visted = set()
-            while q:
-                
-                q_len= len(q)
-               
-                for i in range(q_len):
-                    p, p_color = q.popleft()
-                    
-                    
-                    
-                    for child in graph[p]:
-                        c,c_color = child
-                        
-                        if (c, c_color) not in visted:
-                            if p_color != c_color:
-                                visted.add((c, c_color))
-                                q.append([c, c_color])
-                                if ans[c] == -1:
-                                    
-                                    ans[c] = dis
-                dis +=1
-                   
+        queue.append((0, "r"))
+        queue.append((0, "b"))
+        ans[0] = 0
+        while queue:
             
-        bfs()
+            q_len= len(queue)
+            
+            for _ in range(q_len):
+                p, p_color = queue.popleft()
+                
+                for c, c_color in graph[p]:
+                    if (c, c_color) not in visted:
+                        
+                        if p_color != c_color:
+                            visted.add((c, c_color))
+                            queue.append((c, c_color))
+
+                            if ans[c] == -1:
+                                ans[c] = lev
+                            
+            lev+=1
         return ans
+            
+            
+
+            
         
+        
+        
+                            
+                            
+                            
+                        
+                            
+                            
+        
+                            
+                            
+                            
+                            
