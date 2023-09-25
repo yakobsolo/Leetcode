9d36c2adc7a63145cb70246ea3a824e252c7182e
yakob solomon <yakisolo2009@gmail.com>
2023-09-25 16:14:15 +0300

    Time: 2267 ms (12.81%), Space: 16.6 MB (67.61%) - LeetHub

--- 1626-best-team-with-no-conflicts/1626-best-team-with-no-conflicts.py
+++ 1626-best-team-with-no-conflicts/1626-best-team-with-no-conflicts.py
@@ -8,7 +8,6 @@ class Solution:
             agess.append((ages[i], scores[i]))
         
         agess.sort()
-        # print(agess)
         dp[0] = agess[0][1]
         
         
@@ -17,10 +16,8 @@ class Solution:
             for j in range(i-1,-1, -1):
                 
                 if agess[i][1] >= agess[j][1]  or agess[i][0] == agess[j][0]:
-                    # print(i, j)
                     dp[i] = max(dp[i], dp[j] + agess[i][1])
-                    # print(dp[i] , agess[i][1], dp[j])
                 else:
                     dp[i] = max(dp[i], agess[i][1])
-        # print(dp)
+        
         return max(dp)
