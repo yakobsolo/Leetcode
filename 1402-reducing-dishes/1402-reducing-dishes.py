fc47d723580557090b28af1dc6e835d086cbc3fd
yakob solomon <yakisolo2009@gmail.com>
2023-09-25 17:24:05 +0300

    Time: 197 ms (24.49%), Space: 16.2 MB (97.11%) - LeetHub

--- 1402-reducing-dishes/1402-reducing-dishes.py
+++ 1402-reducing-dishes/1402-reducing-dishes.py
@@ -1,15 +1,14 @@
 class Solution:
     def maxSatisfaction(self, satisfaction: List[int]) -> int:
-        max_sum = 0
-        n = len(satisfaction)
+        N = len(satisfaction)
+        mx = 0
         satisfaction.sort()
-        for i in range(n):
-            j = 1
-            sums = 0
-            for k in range(i, n):
-                sums+=satisfaction[k]*j
-                j+=1
-            max_sum = max(max_sum, sums)
-        if max_sum<0:
-            return 0
-        return max_sum
+        for i in range(N):
+            cur = 0
+            k = 1
+            for j in range(i, N):
+                cur +=(satisfaction[j]*(k))
+                mx = max(mx, cur)
+                k+=1
+            
+        return mx
