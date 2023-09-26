7b8f2e32adb0f3bb5ec4528cdad87f38543acfc8
yakob solomon <yakisolo2009@gmail.com>
2023-09-26 16:06:39 +0300

    Time: 118 ms (5.06%), Space: 29.7 MB (5.78%) - LeetHub

--- 0139-word-break/0139-word-break.py
+++ 0139-word-break/0139-word-break.py
@@ -1,12 +1,61 @@
+class TrieNode:
+    def __init__(self):
+        self.children  = [None]*26
+        self.isEndofWord = False
+            
+class Trie:
+    def __init__(self):
+        self.root = TrieNode()
+        
+    
+    def insert(self, word: str) -> None:
+        cur = self.root
+        for w in word:
+            idx = ord(w)-ord('a')
+            if cur.children[idx] == None:
+                cur.children[idx] = TrieNode()
+                
+            cur  = cur.children[idx]
+        cur.isEndofWord = True
 
+    def search(self, word: str) -> bool:
+        cur = self.root
+        for w in word:
+            idx = ord(w)-ord("a")
+            if cur.children[idx]!=None:
+                cur= cur.children[idx]
+            else: return False
+            
+        return cur.isEndofWord
+                
+                
+   
+            
 class Solution:
     def wordBreak(self, s: str, wordDict: List[str]) -> bool:
-        dp = [False] * (len(s) + 1)
-        dp[-1] = True
-        for i in range(len(s), -1, -1):
-            for word in wordDict:
-                if i+len(word) <= len(s) and s[i:i+len(word)] == word:
-                    dp[i] = dp[i+len(word)]
-                if dp[i]:
-                    break
-        return dp[0]
+        dic = Trie()
+        for word in wordDict:
+            dic.insert(word)
+        N= len(s)
+        memo = {}
+        def dp(S, idx):
+            if idx == N:
+                if dic.search(S):
+                    return True
+                return False
+                
+                
+            cur = S+s[idx]
+            
+            if (cur, idx) in memo:
+                return memo[(cur, idx)]
+            
+            if dic.search(cur):
+                if dp("", idx+1):
+                    memo[(cur, idx)] = True
+                    return memo[(cur, idx)]
+            memo[(cur, idx)] = dp(cur, idx+1)     
+            return memo[(cur, idx)]
+        return dp("", 0)
+                
+                
