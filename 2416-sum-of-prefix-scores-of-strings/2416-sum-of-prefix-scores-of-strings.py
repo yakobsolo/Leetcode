41fc6eaf813ac3f3e34a3dd04142c8940d92d8ba
yakob solomon <yakisolo2009@gmail.com>
2023-09-29 18:59:12 +0300

    Time: 5336 ms (7.16%), Space: 448 MB (33.21%) - LeetHub

--- 2416-sum-of-prefix-scores-of-strings/2416-sum-of-prefix-scores-of-strings.py
+++ 2416-sum-of-prefix-scores-of-strings/2416-sum-of-prefix-scores-of-strings.py
@@ -6,7 +6,7 @@ class TrieNode:
 class Trie:
     def __init__(self):
         self.root = TrieNode()
-        self.prefixScores  = {}
+        self.memo  = {}
     
     def insert(self, word: str) -> None:
         cur = self.root
@@ -19,24 +19,26 @@ class Trie:
             cur.count+=1
 
   
-    def prefixScore(self, word: str) -> None:
-        if word not in self.prefixScores:
-            self.prefixScores[word] = 0
-            currentNode = self.root
+    def calcPrefix(self, word: str) -> None:
+        if word not in self.memo:
+            self.memo[word] = 0
+            cur = self.root
             for char in word:
                 idx = ord(char)-ord("a")
-                currentNode = currentNode.children[idx]
-                self.prefixScores[word] += currentNode.count
-        return self.prefixScores[word]        
-                
-    def startsWith(self, prefix: str) -> bool:
-        cur = self.root
-        for w in prefix:
-            idx = ord(w)-ord("a")
-            if cur.children[idx]!=None:
-                cur= cur.children[idx]
-            else: return 0
-        return cur.count
+                cur = cur.children[idx]
+                self.memo[word] += cur.count
+        return self.memo[word]   
+    
+    # searching all prefix will be O(N3)
+    
+    # def startsWith(self, prefix: str) -> bool:
+    #     cur = self.root
+    #     for w in prefix:
+    #         idx = ord(w)-ord("a")
+    #         if cur.children[idx]!=None:
+    #             cur= cur.children[idx]
+    #         else: return 0
+    #     return cur.count
             
 class Solution:
     def sumPrefixScores(self, words: List[str]) -> List[int]:
@@ -45,7 +47,7 @@ class Solution:
             trie.insert(word)
        
         
-        ans = [trie.prefixScore(word) for word in words]
+        ans = [trie.calcPrefix(word) for word in words]
         return ans       
                 
         
