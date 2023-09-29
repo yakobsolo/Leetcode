2638e3612f2d3eeec11911e2518d5f8685c0e576
yakob solomon <yakisolo2009@gmail.com>
2023-09-30 00:08:20 +0300

    Time: 218 ms (78.72%), Space: 16.7 MB (62.39%) - LeetHub

--- 2646-minimize-the-total-price-of-the-trips/2646-minimize-the-total-price-of-the-trips.py
+++ 2646-minimize-the-total-price-of-the-trips/2646-minimize-the-total-price-of-the-trips.py
@@ -1,44 +1,51 @@
 class Solution:
     def minimumTotalPrice(self, n: int, edges: List[List[int]], price: List[int], trips: List[List[int]]) -> int:
-        adj = defaultdict(list)
+        graph = defaultdict(list)
         for u, v in edges:
-            adj[u].append(v)
-            adj[v].append(u)
+            graph[u].append(v)
+            graph[v].append(u)
         
-        trip_price = [0]*n
-
-        end = -1        
-        def dfs(v, p):
-            if v == end:
-                trip_price[v] += price[v] 
+        finalPrice = [0]*n
+        end = -1
+        def path(node, p):
+            if node == end:
+                finalPrice[node] += price[node]
                 return True
-            for ch in adj[v]:
-                if ch == p: continue
-                if dfs(ch, v):
-                    trip_price[v] += price[v]
+            
+            for child in graph[node]:
+                if child == p: continue
+                if path(child, node):
+                    finalPrice[node] += price[node]
                     return True
             return False
-        
-        for st, en in trips:
-            end = en
-            dfs(st, -1)
-
-        dp = {}    
-
-        def find(v, ok, p):
-            if (v, ok) in dp:
-                return dp[(v, ok)]
-            take = leave = 0
+                    
+                
+        for s, e in trips:
+            end = e
+            path(s, -1)
+            
+        dp = {}
+        def find(node, ok, p):
+            if (node, ok) in dp: return dp[(node, ok)]
+            take, leave = 0, 0
             if ok:
-                take = trip_price[v]//2
-                for ch in adj[v]:
-                    if ch == p: continue
-                    take += find(ch, False, v)
-            for ch in adj[v]:
-                if ch == p: continue
-                leave += find(ch, True, v)
-            dp[(v, ok)] = max(take, leave)
-            return dp[(v, ok)]
-
-        ans = sum(trip_price) - find(0, True, -1)
-        return ans
+                take = finalPrice[node]//2
+                
+                for child in graph[node]:
+                    if child == p: continue
+                    take+=find(child, False, node)
+                    
+            for child in graph[node]:
+                if child == p: continue
+                leave+=find(child, True, node)
+            
+            dp[(node, ok)] = max(take, leave)
+            return dp[(node, ok)]
+                    
+            
+            
+            
+        
+        ans = find(0, True, 0)
+        return sum(finalPrice)-ans
+        
