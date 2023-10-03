dda09eb45f2665010dcd4e756d3fc6d3efacd74e
yakob solomon <yakisolo2009@gmail.com>
2023-10-03 12:09:12 +0300

    Time: 407 ms (81.41%), Space: 18.8 MB (17.39%) - LeetHub

---
+++ 0743-network-delay-time/0743-network-delay-time.py
@@ -0,0 +1,37 @@
+class Solution:
+    def networkDelayTime(self, times: List[List[int]], n: int, k: int) -> int:
+        graph = defaultdict(list)
+        distances = {node: inf for node in range(1, n+1)}
+        for s, d, t in times:
+            # print(s)
+            graph[s].append((d, t))
+            
+#         print(graph[k]) 
+#         if len(graph[k]) == 0: return -1
+            
+        def dijkstra(graph, start):
+            # distances = {node: float('inf') for node in graph}
+            distances[start] = 0
+            visited = set()
+            q = [(0, start)]
+            while q:
+                cur_dis, cur_nod = heapq.heappop(q)
+                
+                if cur_nod in visited:
+                    continue
+                visited.add(cur_nod)
+                for child, wht in graph[cur_nod]:
+                    dis = cur_dis + wht
+                    # print(distances, child)
+                    if dis < distances[child]:
+                        distances[child] = dis
+                        heapq.heappush(q, (dis, child))
+            return distances
+        dis = dijkstra(graph, k)
+        
+        mx = 0
+        for node in dis:
+            if dis[node] == inf:
+                return -1
+            mx = max(mx, dis[node])
+        return mx
