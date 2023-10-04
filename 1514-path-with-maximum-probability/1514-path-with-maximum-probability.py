ba25dbe251a72c7d42f9c87d45eeaf8dced4da28
yakob solomon <yakisolo2009@gmail.com>
2023-10-04 22:30:40 +0300

    Time: 602 ms (58.56%), Space: 27.7 MB (98.26%) - LeetHub

---
+++ 1514-path-with-maximum-probability/1514-path-with-maximum-probability.py
@@ -0,0 +1,35 @@
+class Solution:
+    def maxProbability(self, n: int, edges: List[List[int]], succProb: List[float], start_node: int, end_node: int) -> float:
+        
+        graph= defaultdict(list)
+        i = 0
+        for u, v in edges:
+            graph[u].append([v, succProb[i]])
+            graph[v].append([u , succProb[i]])
+            i+=1
+        
+        heap = []
+        dist = {node: 0 for node in range(n)}
+        heappush(heap, [-1, start_node])
+        
+        visted=  set()
+        
+        while heap:
+            
+            cur_prob, cur_node = heappop(heap)
+           
+            
+            if cur_node == end_node: return -cur_prob
+            
+            for child , succ in graph[cur_node]:
+                
+                prob  = succ*-cur_prob
+                # print(child, prob)
+                
+                if prob>dist[child]: 
+                    dist[child] = prob
+                    heappush(heap, [-prob, child])
+            #     print(heap)
+            # print(dist)
+        return 0
+        
