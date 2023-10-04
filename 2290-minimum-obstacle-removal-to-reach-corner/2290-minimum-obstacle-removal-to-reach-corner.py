6252bc3123891f1750d458d78942ce25b5ec00c4
yakob solomon <yakisolo2009@gmail.com>
2023-10-04 18:58:13 +0300

    Time: 3475 ms (37.95%), Space: 40.4 MB (82.45%) - LeetHub

--- 2290-minimum-obstacle-removal-to-reach-corner/2290-minimum-obstacle-removal-to-reach-corner.py
+++ 2290-minimum-obstacle-removal-to-reach-corner/2290-minimum-obstacle-removal-to-reach-corner.py
@@ -2,23 +2,22 @@ class Solution:
     def minimumObstacles(self, grid: List[List[int]]) -> int:
         n , m = len(grid), len(grid[0])
         direction= [(-1, 0), (0, 1), (1, 0), (0, -1)]
+        dist = [[float("inf")]*m for i in range(n)]
+        
         def isValid(x, y):
             return (0<=x<n and 0<=y<m)
+        dist[0][0] = grid[0][0]
+        heap = [[dist[0][0], 0, 0]]
         
-        heap = [[0, 0, 0]]
-        ans = 0
-        visted = set()
         while heap:
             
             obs , r, c = heappop(heap)
             
-            ans = max(obs, ans)
-            if (r, c) == (n-1, m-1): return ans
-            if (r, c) in visted: continue
-            visted.add((r,c))
+            if (r, c) == (n-1, m-1): return obs
+            
             for i in range(4):
                 x, y = r+direction[i][0], c+direction[i][1]
                 
-                if isValid(x, y) and (x, y) not in visted :
-                    num_obs = max(obs, grid[x][y]+obs)
-                    heappush(heap,[num_obs, x, y])
+                if isValid(x, y) and grid[x][y] + obs < dist[x][y]:
+                    dist[x][y] = grid[x][y] + obs
+                    heappush(heap,[dist[x][y], x, y])
