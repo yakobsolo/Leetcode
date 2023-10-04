b266a477b163f8e8cf1c63bbe59314720cf4f800
yakob solomon <yakisolo2009@gmail.com>
2023-10-04 21:07:14 +0300

    Time: 650 ms (48.05%), Space: 17.6 MB (92.25%) - LeetHub

--- 1631-path-with-minimum-effort/1631-path-with-minimum-effort.py
+++ 1631-path-with-minimum-effort/1631-path-with-minimum-effort.py
@@ -1,33 +1,23 @@
 class Solution:
     def minimumEffortPath(self, heights: List[List[int]]) -> int:
-        dire = {
-            'up': (-1, 0),
-            'down': (1, 0),
-            'left': (0, -1),
-            'right': (0, 1)
-        }
-        m = len(heights)
-        n = len(heights[0])
+        n , m = len(heights), len(heights[0])
+        direction= [(-1, 0), (0, 1), (1, 0), (0, -1)]
+        dist = [[float("inf")]*m for i in range(n)]
         
-        heap = [(0, (0,0))]
-        visted = set()
-        dist = [[float('inf') for i in range(n)] for j in range(m)]
-        # print(dist)
+        def isValid(x, y):
+            return (0<=x<n and 0<=y<m)
+        # dist[0][0] = heights[0][0]
+        heap = [[0, 0, 0]]
+        
         while heap:
-            weight, node = heapq.heappop(heap)
-            if node == (m-1, n-1):
-                return weight
-            visted.add((weight, node))
             
-            # print(node, weight)
-            for i in dire:
-                x = node[0] + dire[i][0]
-                y = node[1] + dire[i][1]
-                if 0 <= x < m and 0 <= y < n:
-                    tmp = max(weight, abs(heights[node[0]][node[1]] - heights[x][y]))
-                    if (tmp, (x, y)) not in visted:
-                        if tmp< dist[x][y]:
-                            dist[x][y] = tmp
-                            heappush(heap, (tmp, (x,y)))
+            obs , r, c = heappop(heap)
+            
+            if (r, c) == (n-1, m-1): return obs
+            
+            for i in range(4):
+                x, y = r+direction[i][0], c+direction[i][1]
                 
-                
+                if isValid(x, y) and max(abs(heights[r][c] -heights[x][y]),  obs) < dist[x][y]:
+                    dist[x][y] = max(abs(heights[r][c] -heights[x][y]),  obs)
+                    heappush(heap,[dist[x][y], x, y])
