42e0701f7c6781410a5cefc1c7d699e13e9b90b9
yakob solomon <yakisolo2009@gmail.com>
2023-10-04 22:11:15 +0300

    Time: 1754 ms (12.59%), Space: 38.8 MB (9.73%) - LeetHub

--- 0239-sliding-window-maximum/0239-sliding-window-maximum.py
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.py
@@ -3,7 +3,6 @@ class Solution:
         heap = []
         N = len(nums)
         counter_window = defaultdict(int)
-        window = set()
         l = 0
         ans = []
         for i in range(N):
@@ -17,19 +16,16 @@ class Solution:
                     heappush(heap, -mx)
                    
                 if counter_window[mx] >0:
-                    ans.append(mx)
-                    
-                    
-                else:
-                    
+                    ans.append(mx)    
+                else: 
                     while counter_window[mx] == 0 :
                         mx = -heappop(heap)
                     ans.append(mx)
                     if nums[l] != mx:
                         heappush(heap, -mx)
+                        
                 counter_window[nums[l]]-=1
                 l+=1
-                # print(nums[i], nums[l-1], counter_window)
                         
         return ans
                 
