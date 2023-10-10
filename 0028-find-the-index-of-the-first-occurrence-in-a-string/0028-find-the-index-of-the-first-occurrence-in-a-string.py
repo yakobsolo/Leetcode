50233383be628ee480aac654f0e9a04647702c15
yakob solomon <yakisolo2009@gmail.com>
2023-10-10 15:33:31 +0300

    Time: 47 ms (5.95%), Space: 16.2 MB (51.80%) - LeetHub

--- 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.py
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.py
@@ -1,10 +1,45 @@
 class Solution:
     def strStr(self, haystack: str, needle: str) -> int:
-        # haystack =  list(haystack)
-        # needle = list(needle)
-        n, N = len(needle), len(haystack)
-        for i in range(N-n+1):
-            # print(haystack[i:n+i])
-            if haystack[i] == needle[0] and haystack[i:n+i] == needle:
-                return i
+        n = len(needle)
+        i =n-1
+        hash = 0
+        N = len(haystack)
+        if i+1>N: return -1
+        for c in needle:
+            hash += (26**i) *ord(c)
+            i-=1
+            
+            hash%=(10**9+7)
+        j =i = n-1
+        find = 0
+        w= 0
+        indx = 0
+        # print(hash)
+        for c in haystack:
+            if w<=i:
+                find += (26**j) * ord(c)
+                w+=1
+                j-=1
+                find %=(10**9+7)
+                
+                if w>i and find == hash:
+                    return indx
+                # print(find, w, i)
+            else:
+                minus = ((26**i)*ord(haystack[indx]))
+                find -= minus
+                find *=26 
+                find += ord(c)
+                # print(c, find, minus)
+                # find*=26
+                find%=(10**9+7)
+                indx+=1
+                if find == hash:
+                    return indx
         return -1
+                
+                
+                
+            
+            
+            
