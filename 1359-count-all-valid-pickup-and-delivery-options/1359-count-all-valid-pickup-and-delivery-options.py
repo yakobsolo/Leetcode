eaf90c1122a572f82ab7754d9cf3e2ba1441f2a6
yakob solomon <yakisolo2009@gmail.com>
2023-10-13 10:19:03 +0300

    Time: 32 ms (91.15%), Space: 16.1 MB (89.24%) - LeetHub

--- 1359-count-all-valid-pickup-and-delivery-options/1359-count-all-valid-pickup-and-delivery-options.py
+++ 1359-count-all-valid-pickup-and-delivery-options/1359-count-all-valid-pickup-and-delivery-options.py
@@ -1,13 +1,6 @@
 class Solution:
     def countOrders(self, n: int) -> int:
-        res = 1
+        
         mod  = 10**9 + 7
-        k = n*2
-        # if n==1:
-        #     return 1
-        for _ in range(n*2):
-            # print(k)
-            res  =(res*k)
-            k-=1
-        return (res//(pow(2, n)))%mod
+        return (factorial(n*2)//(pow(2, n)))%mod
         
