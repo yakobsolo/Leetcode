445093e0fc2f6415fd80b8522f52fe8aaf47cede
yakob solomon <yakisolo2009@gmail.com>
2023-10-13 10:08:47 +0300

    Time: 30 ms (95.21%), Space: 16.2 MB (89.24%) - LeetHub

--- 1359-count-all-valid-pickup-and-delivery-options/1359-count-all-valid-pickup-and-delivery-options.py
+++ 1359-count-all-valid-pickup-and-delivery-options/1359-count-all-valid-pickup-and-delivery-options.py
@@ -3,10 +3,8 @@ class Solution:
         res = 1
         mod  = 10**9 + 7
         k = n*2
-        # if n==1:
-        #     return 1
+       
         for _ in range(n*2):
-            # print(k)
             res  =(res*k)
             k-=1
         return (res//(pow(2, n)))%mod
