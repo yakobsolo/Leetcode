d4f4f9d8d9ef6bf867f5f7ef94d081ff2b530189
yakob solomon <yakisolo2009@gmail.com>
2023-10-18 10:32:51 +0300

    Time: 1849 ms (12.68%), Space: 17.4 MB (5.83%) - LeetHub

---
+++ 0839-similar-string-groups/0839-similar-string-groups.py
@@ -0,0 +1,42 @@
+class Solution:
+    def numSimilarGroups(self, strs: List[str]) -> int:
+        rep = {c:c for c in strs}
+        rank = {c:1 for c in strs}
+        
+        def find(s):
+            if rep[s] == s:
+                return s
+            rep[s] = find(rep[s])
+            return rep[s]
+    
+        def union(c1, c2):
+            par1 = find(c1)
+            par2 = find(c2)
+            
+            if par1!= par2:
+                if rank[par1] >= rank[par2]:
+                    rep[par2] = par1
+                    rank[par1]+=1
+                else:
+                    rep[par1] = par2
+                    rank[par2]+=1
+            return 
+        
+        
+        N = len(strs)
+        for i in range(N):
+            for j in range(i, N):
+                
+                if len(strs[i]) == len(strs[j]):
+                    c= 0
+                    for k in range(len(strs[i])):
+                        if strs[i][k] != strs[j][k]:
+                            c+=1
+                    if c<=2:
+                        union(strs[i], strs[j])
+        
+        hash = defaultdict(int)
+        for c in strs:
+            hash[find(c)] +=1
+        return len(hash)
+            
