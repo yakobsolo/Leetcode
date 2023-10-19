f73f5ba206e6ee08f6acdcaae0457ef346975de4
yakob solomon <yakisolo2009@gmail.com>
2023-10-19 21:19:47 +0300

    Time: 49 ms (31.06%), Space: 16.3 MB (42.05%) - LeetHub

--- 0507-perfect-number/0507-perfect-number.py
+++ 0507-perfect-number/0507-perfect-number.py
@@ -1,7 +1,15 @@
 class Solution:
     def checkPerfectNumber(self, num: int) -> bool:
-        primes = {2, 3, 5, 7, 13, 17, 19, 31}
-        for item in primes:
-            if (2**(item-1))*((2**item)-1) == num:
-                return True
+        sm = 1
+        if num==1:
+            return False
+    
+        
+        i = 2
+        while i<=sqrt(num):
+            if num%i == 0:
+                sm += i+num//i
+            i+=1
+        if sm == num:
+            return True
         return False
